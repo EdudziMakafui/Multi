fe873b2c21bfb515fe2dd07806d8ca63c4cc41b2
Edudzi Makafui Akpabey <77601649+EdudziMakafui@users.noreply.github.com>
2022-10-28 20:17:36 +0000

    init

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php include("includes/header.php");?>
+
+  <!-- ======= Header ======= -->
+  <?php include("includes/header_nav.php");?>
+<!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <?php include("includes/hero_section.php");?>
+<!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <?php include("includes/about_section.php");?>
+ <!-- End About Section -->
+
+    <!-- ======= Counts Section ======= -->
+    <?php include("includes/counts_section.php");?>
+  <!-- End Counts Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <?php include("includes/why_us_section.php");?>
+   <!-- End Why Us Section -->
+
+    <!-- ======= Services Section ======= -->
+    <?php include("includes/services_section.php");?>
+<!-- End Services Section -->
+
+    <!-- ======= Testimonials Section ======= -->
+    <?php include("includes/testmonial_section.php");?>
+  <!-- End Testimonials Section -->
+
+    <!-- ======= Call to action Section ======= -->
+    <?php include("includes/call_to_action.php");?>
+   <!-- End Cta Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <?php include("includes/portfolio_section.php");?>
+  <!-- End Portfolio Section -->
+
+    <!-- ======= Team Section ======= -->
+    <?php include("includes/team_section.php");?>
+  <!-- End Team Section -->
+
+    <!-- ======= Pricing Section ======= -->
+    <?php include("includes/pricing_section.php");?>
+   <!-- End Pricing Section -->
+
+    <!-- ======= Frequently Asked Questions Section ======= -->
+    <?php include("includes/fqa_section.php");?>
+ <!-- End Frequently Asked Questions Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <?php include("includes/contact_section.php");?>
+ <!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+<?php include("includes/footer.php");?>
